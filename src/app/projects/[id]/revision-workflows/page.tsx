f5bfdac89069a88d9dcfba6826dd06b5a890030f
clement893 <clement@nukleo.com>
2025-12-15 16:06:42 -0500

    Fix: Prefix unused onUpdate parameter with underscore in WorkflowDetailModal

--- src/app/projects/[id]/revision-workflows/page.tsx
+++ src/app/projects/[id]/revision-workflows/page.tsx
@@ -357,7 +357,7 @@ export default function RevisionWorkflowsPage() {
 function WorkflowDetailModal({
   workflow,
   onClose,
-  onUpdate,
+  onUpdate: _onUpdate,
 }: {
   workflow: RevisionWorkflow;
   onClose: () => void;
